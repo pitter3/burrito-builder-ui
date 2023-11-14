6925a7577f16f61fc8b71e7a59553888ab7edf5b
Patrick Eitel <patrickeitel12@gmail.com>
2023-11-14 10:11:21 -0800

    Add assertions forfirst and last ingredients for adding a new burrito. Added another ingredient, and tested the Order: field

--- cypress/e2e/add-order.cy.js
+++ cypress/e2e/add-order.cy.js
@@ -10,7 +10,7 @@ describe('Submit Form with Bob and Beans', () => {
       body: {
         id: 4, 
         name: 'Bob',
-        ingredients: ['beans'],
+        ingredients: ['beans', 'pico de gallo'],
       },
     }).as('postOrder');
   });
@@ -22,9 +22,17 @@ describe('Submit Form with Bob and Beans', () => {
     cy.get('.Form input[name="name"]').type('Bob');
     cy.get('.Form input[name="name"]').should('have.value', 'Bob');
     cy.get('.Form button:contains("beans")').click();
+    cy.get('.Form p').contains('Order: beans').should('exist');
+    cy.get('.Form button:contains("pico de gallo")').click();
+    cy.get('.Form p').contains('Order: beans, pico de gallo').should('exist');
     cy.get('.Form button[type="submit"]').click();
     cy.wait('@postOrder');
     cy.get('.order').should('have.length', 4); 
+    cy.get('.order').first().find('h3').should('contain', 'Pat');
+    cy.get('.order').first().find('ul.ingredient-list li').first().should('contain', 'beans');
+    cy.get('.order').first().find('ul.ingredient-list li').last().should('contain', 'jalapeno');
     cy.get('.order').last().find('h3').should('contain', 'Bob');
+    cy.get('.order').last().find('ul.ingredient-list li').first().should('contain', 'beans');
+    cy.get('.order').last().find('ul.ingredient-list li').last().should('contain', 'pico de gallo');
   });
 });
