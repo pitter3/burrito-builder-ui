68b60073f18dd68fc9f78d496f436eda47b0e52e
Patrick Eitel <patrickeitel12@gmail.com>
2023-11-14 09:59:11 -0800

    Add testing for first and last ingredients of first and last cards on homepage. Add testing for first and last buttons in the form button section

--- cypress/e2e/home-page.cy.js
+++ cypress/e2e/home-page.cy.js
@@ -12,10 +12,16 @@ describe('Home Page', () => {
     cy.get('.Form input[name="name"]').should('exist');
     cy.get('.Form input[name="name"]').should('have.attr', 'placeholder', 'Name');
     cy.get('.Form button').should('have.length', 13);
+    cy.get('.Form button').first().should('contain', 'beans');
+    cy.get('.Form button').last().should('contain', 'Submit Order');
     cy.get('.Form p').contains('Order: Nothing selected').should('exist');
     cy.get('.Form button[type="submit"]').should('exist');
     cy.get('.order').should('have.length', 3);
     cy.get('.order').first().find('h3').should('contain', 'Pat');
+    cy.get('.order').first().find('ul.ingredient-list li').first().should('contain', 'beans');
+    cy.get('.order').first().find('ul.ingredient-list li').last().should('contain', 'jalapeno');
     cy.get('.order').last().find('h3').should('contain', 'Alex');
+    cy.get('.order').last().find('ul.ingredient-list li').first().should('contain', 'sofritas');
+    cy.get('.order').last().find('ul.ingredient-list li').last().should('contain', 'queso fresco');
   });
 });
